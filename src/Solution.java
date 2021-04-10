31dfe1bf3fc0dec8d071e51b874013b10596b317
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-11 00:56:20 +0530

    solution 1

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
 public class Solution {
-}
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            if(n%2!=0){
+                System.out.println("Weird");
+            }
+            else{
+                if(n>=2&&n<=5){
+                    System.out.println("Not Weird");
+                }
+                if(n>=6&&n<=20){
+                    System.out.println("Weird");
+                }
+                if (n > 20) {
+                    System.out.println("Not Weird");
+                }
+            }
+
+
+        }
+    }
+
