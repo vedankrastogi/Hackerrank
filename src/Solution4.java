9c7d2659961f2408914803522bec15790b5a3983
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-12 01:40:27 +0530

    solution 4

---
+++ src/Solution4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution4 {
+        static Scanner sc=new Scanner(System.in);
+        static int B=sc.nextInt();
+        static int H=sc.nextInt();
+    public static void main(String[] args){
+        boolean flag = false;
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
+
